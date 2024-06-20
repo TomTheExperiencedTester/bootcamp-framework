bb8da4654c1725cefc569a397da11343a271c47f
TomTheExperiencedTester <tom.vandesteene@polteq.com>
2024-06-20 15:11:29 +0200

    bootcamp-framework

---
+++ src/main/java/polteq/test/Main.java
@@ -0,0 +1,7 @@
+package polteq.test;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,62 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FirstSeleniumTest {
+
+    private WebDriver driver;
+    private final String BASE_URL = "https://greatshop.polteq-testing.com/";
+
+    @BeforeMethod
+    public void setUp() {
+        // Set up ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testHomePageTitle() {
+        // Navigate to the website
+        driver.get(BASE_URL);
+        // Check the title of the page
+        String PAGE_TITLE = "Polteq Mission Critical Testing - TEST webshop - Polteq's Great TestShop";
+        Assert.assertEquals(driver.getTitle(), PAGE_TITLE);
+    }
+
+    @Test
+    public void testLoginSuccessful() {
+        // Navigate to the website
+        driver.get(BASE_URL);
+        driver.findElement(By.className("login")).click();
+        WebElement emailAddressTextbox = driver.findElement(By.id("email"));
+        emailAddressTextbox.sendKeys("tom.vandesteene@polteq.com");
+        WebElement PasswordTextbox = driver.findElement(By.id("passwd"));
+        PasswordTextbox.sendKeys("Nina2");
+        WebElement signinButton = driver.findElement(By.id("SubmitLogin"));
+        signinButton.click();
+        List<WebElement> signoutButtons = driver.findElements(By.className("logout"));
+        Assert.assertEquals(signoutButtons.size(), 1);
+    }
+
+    @Test
+    public void testLoginUnsuccessful() {}
+
+    @AfterMethod
+    public void tearDown() {
+        //Close the browser
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
